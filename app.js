0c61a66ec2b5e4ae46e3e24501adfa645e573b10
syh <dbsgur6896@naver.com>
2021-08-22 12:39:40 +0900

    0822

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
-const app = express();
 const indexRouter = require('./routes');
 const userRouter = require('./routes/user');
 
+const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 
 app.use(morgan('dev'));  //기존 로그외에 추가적인 로그를 볼 수 있음!
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
@@ -33,55 +35,16 @@ app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('Not Found');
-});
-
-const multer = require('multer');
-const fs = require('fs');
-
-try {
-    fs.readdirSync('uploads');
-} catch (error) {
-    console.log('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('uploads');
-}
-const upload = multer({
-    storage: multer.diskStorage({
-        destination(req, file, done) {
-            done(null, 'uploads/');
-        },
-        filename(req, file, done) {
-            const ext = path.extname(file.originalname);
-            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-        },
-    }),
-    limits: {fileSize: 5 * 1024 * 1024},
-});
-
-app.get('/upload', (req, res) => {
-    res.sendFile(path.join(__dirname, 'multipart.html'));
-});
-app.post('/upload',
-    upload.fields([{name: 'image1'}, {name: 'image2'}]),
-    (req, res) => {
-        console.log(req.files, req.body);
-        res.send('ok');
-    });
-
-app.use((req, res, next) => {
-    console.log('모든 요청에 다 실행됩니다.');
-    next();
-})
-app.get('/', (req, res, next) => {
-    console.log('GET / 요청에서만 실행됩니다.');
-    next();
-}, (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
 });
 
 app.use((err, req, res, next) => {
-    console.error('err');
-    res.status(500).send(err.message);
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(app.get('port'), () => {
